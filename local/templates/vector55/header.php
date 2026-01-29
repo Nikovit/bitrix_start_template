e98cc396b574900453c25f1f162c7d910ba2aa50
Nikolaev <info@nikovit.ru>
2026-01-29 11:28:10 +0600

    Добавил библиотеки в vendor и подключил

--- local/templates/vector55/header.php
+++ local/templates/vector55/header.php
@@ -7,7 +7,36 @@ IncludeTemplateLangFile(__FILE__);
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <title><? $APPLICATION->ShowTitle() ?></title>
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+
     <? $APPLICATION->ShowHead(); ?>
+    <? $APPLICATION->ShowMeta("robots") ?>
+    <? $APPLICATION->ShowMeta("keywords") ?>
+
+    <?
+
+    use Bitrix\Main\Page\Asset;
+
+    // Bootstrap
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/vendor/bootstrap/css/bootstrap.min.css');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/vendor/bootstrap/js/bootstrap.js');
+    // JS
+    CJSCore::Init(array("jquery"));
+    // STRING
+    Asset::getInstance()->addString("<meta name='viewport' content='width=device-width, initial-scale=1'>");
+    // Fonts
+    Asset::getInstance()->addString("<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800&amp;subset=cyrillic' rel='stylesheet'>");
+    // Swiper-slider
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/vendor/swiper-slider/swiper-bundle.min.css');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/vendor/swiper-slider/swiper-bundle.min.js');
+    //    // Fancybox
+    //    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/vendor/fancybox/fancybox.css');
+    //    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/vendor/fancybox/fancybox.umd.js');
+
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/script.js');
+
+    ?>
+
 </head>
 <body>
 
