aa65097b4d6eb92f31b9adb99de4981b2e56790b
Nikolaev <info@nikovit.ru>
2026-01-29 13:46:54 +0600

    Доработал README.md

---
+++ local/site_template/templates/description.php
@@ -0,0 +1,6 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+$arTemplate = Array(
+	"NAME"=>"site_template",
+	"DESCRIPTION"=>"Шаблон для разработки сайта на битрикс",
+);?>
