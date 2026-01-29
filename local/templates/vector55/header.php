302f8402c835c89f2149023573cda8a069655008
Nikolaev <info@nikovit.ru>
2026-01-29 13:23:22 +0600

    Изменил структуру

--- local/templates/vector55/header.php
+++ local/templates/vector55/header.php
@@ -1,48 +1,43 @@
-<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
-<?
-IncludeTemplateLangFile(__FILE__);
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+use Bitrix\Main\Page\Asset;
+
+Asset::getInstance()->addString('<meta charset="utf-8">');
+Asset::getInstance()->addString('<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">');
+
+// Bootstrap
+Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/bootstrap/css/bootstrap.min.css');
+Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/bootstrap/js/bootstrap.bundle.min.js');
+
+// Swiper-slider
+Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/swiper-slider/swiper-bundle.min.css');
+Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/swiper-slider/swiper-bundle.min.js');
+
+//    // Fancybox
+//    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/fancybox/fancybox.css');
+//    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/fancybox/fancybox.umd.js');
+
+// Fonts
+//Asset::getInstance()->addString("<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800&amp;subset=cyrillic' rel='stylesheet'>");
+
+// JS
+//CJSCore::Init(array("jquery"));
+Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/script.js');
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<html lang="ru">
 <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <title><? $APPLICATION->ShowTitle() ?></title>
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
-
+    <link rel="icon" href="/favicon.svg">
     <? $APPLICATION->ShowHead(); ?>
     <? $APPLICATION->ShowMeta("robots") ?>
     <? $APPLICATION->ShowMeta("keywords") ?>
-
-    <?
-
-    use Bitrix\Main\Page\Asset;
-
-    // Bootstrap
-    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/bootstrap/css/bootstrap.min.css');
-    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/bootstrap/js/bootstrap.js');
-    // JS
-    CJSCore::Init(array("jquery"));
-    // STRING
-    Asset::getInstance()->addString("<meta name='viewport' content='width=device-width, initial-scale=1'>");
-    // Fonts
-    Asset::getInstance()->addString("<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800&amp;subset=cyrillic' rel='stylesheet'>");
-    // Swiper-slider
-    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/swiper-slider/swiper-bundle.min.css');
-    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/swiper-slider/swiper-bundle.min.js');
-    //    // Fancybox
-    //    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/fancybox/fancybox.css');
-    //    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/fancybox/fancybox.umd.js');
-
-    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/script.js');
-
-    ?>
-
 </head>
 <body>
-
+<div id="panel"><? $APPLICATION->ShowPanel(); ?></div>
 <header>
 
 </header>
 
-<div id="panel"><? $APPLICATION->ShowPanel(); ?></div>
+
 
